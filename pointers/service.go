ab7612a746ae491f3fa9e8ea0356913b53769306
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-08-18 09:52:47 -0300

    Add service for pointers and value receivers.

---
+++ pointers/service.go
@@ -0,0 +1,23 @@
+package pointers
+
+import (
+	"github.com/marcoshuck/go-receivers-benchmark/logger"
+)
+
+type Service interface {
+	Do()
+}
+
+type service struct {
+	Logger logger.Logger
+}
+
+func (s *service) Do() {
+	s.Logger.Log("Done.")
+}
+
+func NewService(logger logger.Logger) Service {
+	return &service{
+		Logger: logger,
+	}
+}
